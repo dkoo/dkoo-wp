4361a34a124fdc966f5bd990a5ece4aaa3a0ac34
dkoo <d@dkoo.net>
2016-05-21 15:38:04 -0400

    fix babel task

--- tasks/options/babel.js
+++ tasks/options/babel.js
@@ -1,14 +1,11 @@
 module.exports = {
-	babel: {
-		options: {
-			sourceMap: true,
-			presets: ['es2015']
-		},
-		dist: {
-			files: {
-				'assets/js/dkoo-dot-net.js': 'assets/js/src/dkoo-dot-net.js'
-			}
+	options: {
+		sourceMap: true,
+		presets: ['es2015']
+	},
+	dist: {
+		files: {
+			'assets/js/dkoo-dot-net.js': 'assets/js/src/dkoo-dot-net.js'
 		}
 	}
-
 };
