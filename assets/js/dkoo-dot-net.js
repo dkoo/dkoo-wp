5abeceac705e5fbad4e67b174adec456d00849f5
dkoo <d@dkoo.net>
2016-05-21 15:27:59 -0400

    remove compiled css/js files from repository

--- assets/js/dkoo-dot-net.js
+++
@@ -1,8 +0,0 @@
-/*! dkoo dot net - v0.1.0
- * http://dkoo.net
- * Copyright (c) 2016; * Licensed GPL-2.0+ */
-( function( window, undefined ) {
-	'use strict';
-
-	console.log('js loaded');
-} )( this );
